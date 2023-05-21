e2787a14383bdc814feafb09370bca7c86eb778f
Muhammed Saad Anaikar <saad@Muhammeds-MacBook-Air.local>
2023-05-21 18:36:51 -0400

    developed navbar componenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom';
+import './App.css';
 import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import ExerciseDetail from './pages/ExerciseDetail';
-import './App.css';
+
 const App = () => {
   return (
     <Box width="400px" sx={{ width: {xl: '1488px'}}} m="auto">
